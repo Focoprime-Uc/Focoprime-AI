bbf50b43d07daac583c90c2100945a601f819592
Focoprime Global <gpedromoz4@gmail.com>
2026-03-11 11:04:44 +0200

    Delete public/scripts directory

--- public/scripts/sugere.js
+++
@@ -1,8 +0,0 @@
-// SUGGESTIONS
-// ==============================
-document.querySelectorAll(".suggestions-item").forEach(item => {
-  item.addEventListener("click", () => {
-    promptInput.value = item.querySelector(".text").textContent;
-    promptForm.dispatchEvent(new Event("submit"));
-  });
-});
